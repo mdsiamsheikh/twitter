31bcee29cc09800822dc120950a186103a5554ce
mdsiamsheikh <mdsiamsheikh6640@gmail.com>
2023-01-09 03:54:32 +0600

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <Header></Header>
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+import Header from "../components/header.vue";
+
+export default {
+  name: "IndexPage",
+  components: { Header },
+};
+</script>
+
+<style></style>
---
+++ pages/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="contaner">
+    <middle-sidebar></middle-sidebar>
+    <rightSideberVue></rightSideberVue>
+  </div>
+</template>
+
+<script>
+import middleSidebarVue from "../components/middleSidebar.vue";
+import rightSideberVue from "../components/rightSideber.vue";
+export default {
+  name: "IndexPage",
+  components: {
+    middleSidebarVue,
+    rightSideberVue,
+  },
+};
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  display: flex;
+  height: 100vh;
+  max-width: 1300px;
+  margin-left: auto;
+  margin-right: auto;
+  /* padding: 0 10px; */
+}
+</style>
